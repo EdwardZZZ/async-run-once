e027a60418f0f008406c12c4ee3e5963cacfdb58
大华 <zzh627@126.com>
2019-10-11 09:44:23 +0800

    update d.ts
    return type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export = once;
 
-declare function once(name: Function): Function;
+declare function once(name: Function): (...any: any[]) => Promise<any>;
 
 declare namespace once {
     export let onceMap: Function;
